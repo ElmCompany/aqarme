50357595488e6f737486ccbffe0cc83b52f6af26
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-24 05:55:12 +0300

    configure datasource

--- src/main/java/aqar/AqarApplication.java
+++ src/main/java/aqar/AqarApplication.java
@@ -2,9 +2,14 @@ package aqar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 @EnableScheduling
 @SpringBootApplication
@@ -17,5 +22,12 @@ public class AqarApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
     }
 }
