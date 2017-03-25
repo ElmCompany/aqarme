26165120e7dcc3be605b1f9afb353f647da1373d
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-26 00:23:45 +0300

    clean

--- src/main/java/aqar/AqarService.java
+++ src/main/java/aqar/AqarService.java
@@ -7,7 +7,6 @@ import com.sromku.polygon.Polygon;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
