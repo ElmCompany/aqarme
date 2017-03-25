7950ab49219de08cef616bc461b9dd6a06780b4b
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-25 05:41:18 +0300

    add getter for proccessads

--- src/main/java/aqar/db/ProcessedAds.java
+++ src/main/java/aqar/db/ProcessedAds.java
@@ -1,10 +1,13 @@
 package aqar.db;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 public class ProcessedAds {
 
