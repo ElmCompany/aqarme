48ea07606fdb8137aa68499b0c09c3e63d41c936
Muhammad Hewedy <daziplqa@gmail.com>
2017-03-26 11:36:44 +0300

    Update Scheduler.java

--- src/main/java/aqar/Scheduler.java
+++ src/main/java/aqar/Scheduler.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 public class Scheduler {
 
     private final static int HOUR = 1000 * 60 * 60;
-    private final static int RATE = 6;
+    private final static int RATE = 3;
 
     @Value("${aqar.messenger.senderList}")
     private String[] senderList;
