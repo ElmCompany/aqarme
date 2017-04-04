2694097c682926fe538e9980b427caebee3d8b50
Muhammad Hewedy <mhewedy@elm.sa>
2017-04-04 17:38:14 +0300

    spcs instd of tbs

--- src/main/java/aqar/AqarService.java
+++ src/main/java/aqar/AqarService.java
@@ -82,8 +82,8 @@ class AqarService {
             Elements adsList = getfromUrlAndSleep(currentPageUrl).select(".list-single-adcol");
             
             return adsList.stream()
-					.filter(this::notProcessed)
-					.map(this::detailsPage)
+                    .filter(this::notProcessed)
+                    .map(this::detailsPage)
                     .flatMap(this::multiplex)
                     .filter(this::notProcessed)
                     .filter(this::matchesPrice)
