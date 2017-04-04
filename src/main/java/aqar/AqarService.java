8dd42245525b0acebf2bb074a452dc9a7b303176
Muhammad Hewedy <mhewedy@gmail.com>
2017-04-05 00:48:51 +0300

    keep the fraction of the apart price

--- src/main/java/aqar/AqarService.java
+++ src/main/java/aqar/AqarService.java
@@ -1,14 +1,11 @@
 package aqar;
 
-import static java.lang.Integer.*;
-import static java.util.Collections.*;
-import static java.util.stream.Collectors.*;
-import static java.util.stream.IntStream.*;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
-
+import aqar.model.Advertise;
+import aqar.model.JobElement;
+import aqar.model.repo.AdvertiseRepository;
+import aqar.model.repo.JobRepository;
+import com.sromku.polygon.Point;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,13 +14,14 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.sromku.polygon.Point;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
-import aqar.model.Advertise;
-import aqar.model.JobElement;
-import aqar.model.repo.AdvertiseRepository;
-import aqar.model.repo.JobRepository;
-import lombok.extern.slf4j.Slf4j;
+import static java.lang.Float.parseFloat;
+import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.*;
+import static java.util.stream.IntStream.rangeClosed;
 
 @Slf4j
 @Service
@@ -31,6 +29,7 @@ class AqarService {
 
     private static final String LOC = "loc:";
     private static final String PLUS = "+";
+    private static final int ONE_K = 1000;
 
     @Value("${aqar.base.url}")
     private String baseUrl;
@@ -72,12 +71,12 @@ class AqarService {
             return emptyMap();
         }
     }
-    
+
     // ------------------------------------------------------------------------------------
 
     private Stream<JobElement> getMatchedJobElements(int pageNumber) {
         Elements adsList = getfromUrl(baseUrl + searchUrl + pageNumber).select(".list-single-adcol");
-        
+
         return adsList.stream()
                 .filter(this::notProcessed)
                 .map(this::detailsPage)
@@ -97,9 +96,9 @@ class AqarService {
             adsRepository.markAsSuccess(je.element().select("kbd").text(), je.jobId());
         }
     }
-    
+
     // ------------------------------------------------------------------------------------    
-    
+
     private boolean notProcessed(Element element) {
         String number = extractNumber(element.id());
         if (adsRepository.countOfJobsProcessedAdvertise(number) == jobRepository.count()) {
@@ -108,7 +107,7 @@ class AqarService {
         }
         return true;
     }
-    
+
     private Element detailsPage(Element element) {
         String urlPart = element.select("a").attr("href");
         return getfromUrl(baseUrl + urlPart);
@@ -133,7 +132,7 @@ class AqarService {
 
     private boolean matchesPrice(JobElement je) {
         try {
-            int price = parseInt(extractNumber(je.element().select(".label ,radius").text())) * 1000;
+            int price = (int) (parseFloat(extractNumber(je.element().select(".label ,radius").text())) * ONE_K);
             boolean ret = true;
             if (je.maxPrice() != null) {
                 ret = price <= je.maxPrice();
@@ -150,7 +149,7 @@ class AqarService {
     private boolean hasImage(JobElement je) {
         return !je.hasImages() || !je.element().select("#adcol-images").isEmpty();
     }
-    
+
     private boolean insideSelectedArea(JobElement je) {
         return je.element().select("tr td a")
                 .stream()
@@ -161,7 +160,7 @@ class AqarService {
                 .map(point -> je.polygon().contains(point))
                 .orElse(false);
     }
-    
+
     private boolean hasElevator(JobElement je) {
         return !je.hasElevator() || je.element().select(".single-adcolum").text().contains(elevatorWord);
     }
@@ -184,36 +183,36 @@ class AqarService {
         });
     }
     // ------------------------------------------------------------------------------------------------
-    
+
     private Point toPoint(String url) {
-        float latitude = Float.parseFloat(url.substring(url.indexOf(LOC) + LOC.length(), url.indexOf(PLUS)));
-        float longitude = Float.parseFloat(url.substring(url.indexOf(PLUS) + PLUS.length()));
+        float latitude = parseFloat(url.substring(url.indexOf(LOC) + LOC.length(), url.indexOf(PLUS)));
+        float longitude = parseFloat(url.substring(url.indexOf(PLUS) + PLUS.length()));
         return new Point(latitude, longitude);
     }
 
-	/**
-	 * sleeps for {@link #sleepMillis} before get the document
-	 */
-	private Document getfromUrl(String url) {
-		try {
-			Thread.sleep(sleepMillis);
-			log.info("connect to: {}", url);
-			return Jsoup.connect(url).get();
-		} catch (Exception ex) {
-			if (ex.getCause() instanceof HttpStatusException) {
-				HttpStatusException hse = ((HttpStatusException) ex.getCause());
-				log.error(hse.getStatusCode() + ", " + hse.getUrl() + ", " + hse.getMessage());
-			} else {
-				log.error(ex.getMessage());
-			}
-			return new Document(url);
-		}
-	}
+    /**
+     * sleeps for {@link #sleepMillis} before get the document
+     */
+    private Document getfromUrl(String url) {
+        try {
+            Thread.sleep(sleepMillis);
+            log.info("connect to: {}", url);
+            return Jsoup.connect(url).get();
+        } catch (Exception ex) {
+            if (ex.getCause() instanceof HttpStatusException) {
+                HttpStatusException hse = ((HttpStatusException) ex.getCause());
+                log.error(hse.getStatusCode() + ", " + hse.getUrl() + ", " + hse.getMessage());
+            } else {
+                log.error(ex.getMessage());
+            }
+            return new Document(url);
+        }
+    }
 
     private String extractNumber(String text) {
         return text.replaceAll("[^\\d.]", "");
     }
-    
+
     private String adNumber(JobElement je) {
         return je.element().select("kbd").text();
     }
